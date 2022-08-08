94df06d4f4b1ae816ea42335eeaadf03e69d352d
pakhi <pakhivashis182001@gmail.com>
2022-08-08 16:32:50 +0530

    Literals

---
+++ Literals/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        byte age = 12;
+        int age2 = 44;
+        long age3 = 22;
+        short age4 = 10;
+        char ch = 'P';
+        float f1 = 5.6f;
+        double d1 = 3.56;
+        boolean a = true;
+        System.out.println(age);
+        String str = "Pakhi";
+        System.out.println(str);
+
+    }
+}
